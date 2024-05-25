1490de41c219c728da21df41641915d8f6dbf4b2
Fernando Mateo Valencia Gómez <fmvalencia@uninorte.edu.co>
2024-05-25 13:56:11 -0500

    cosas

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/imageController');
+const {upload} = require('../middleware/multer');
+const auth = require('../middleware/authToken.js');
 
-//Aply image Middleware to request
-const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-router.post('/', upload.single('image'), controllers.createImages);
+//Falta aplicar middleware para obtener id
+router.post('/', upload.array('image',10), controllers.createImages);
 router.get('/', controllers.getImages);
-router.delete('/', controllers.deleteImages);
+router.delete('/', auth.verifyToken, controllers.deleteImages);
+router.put('/:id', auth.verifyToken, controllers.updateText);
 module.exports = router;
